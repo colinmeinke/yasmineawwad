5768988d18ae6d84dc37fb7da387af322829ccda
Colin Meinke <hello@colinmeinke.com>
2021-11-30 18:39:38 +0000

    add padding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   <title>Yasmine Awwad - Writer etc.</title>
 </svelte:head>
 
-<div class="flex flex-col">
+<div class="flex flex-col p-[1rem]">
   <h1 class="font-serif text-[2rem] md:text-[3rem] font-bold tracking-[0.1rem]">Yasmine Awwad</h1>
   <p class="font-sans mt-[0.15rem] md:mt-[0.3rem] text-[1rem] md:text-[1.25rem] tracking-[0.05rem] opacity-[0.9]">Writer etc.</p>
   <p class="opacity-[0.9] font-sans mt-[1.25rem] md:mt-[2.5rem] text-[1rem] md:text-[1.25rem] tracking-[0.05rem]">Contact: <a class="opacity-[0.9] underline hover:opacity-[1] focus:opacity-[1] transition" href="mailto:hello@yasmineawwad.co.uk">hello@yasmineawwad.co.uk</a></p>
